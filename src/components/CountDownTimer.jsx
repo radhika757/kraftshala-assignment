b5c487845e4375f0f3fd75a4e58d4e164c910b62
Radhika Rao <raoradhika2000@gmail.com>
2025-06-26 21:20:15 +0530

    Added timer functionality

--- src/components/CountDownTimer.jsx
+++ src/components/CountDownTimer.jsx
@@ -1,12 +1,48 @@
-
+import { useEffect, useState } from 'react';
 import styles from './CountDownTimer.module.css';
 
 const CountdownTimer = () => {
+const calculateTimeLeft = () => {
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + 8);
+    targetDate.setHours(23, 55, 41); // match image
 
+    const difference = +targetDate - +new Date();
+
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: String(Math.floor(difference / (1000 * 60 * 60 * 24))).padStart(2, '0'),
+        hours: String(Math.floor((difference / (1000 * 60 * 60)) % 24)).padStart(2, '0'),
+        minutes: String(Math.floor((difference / 1000 / 60) % 60)).padStart(2, '0'),
+        seconds: String(Math.floor((difference / 1000) % 60)).padStart(2, '0')
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <div className={styles.timerContainer}>
       <h1>WE'RE LAUNCHING SOON</h1>
+      <div className={styles.timeBoxes}>
+        {Object.entries(timeLeft).map(([label, value]) => (
+          <div key={label} className="time-box">
+            <span className="value">{value}</span>
+            <span className="label">{label.toUpperCase()}</span>
+          </div>
+        ))}
+      </div>
 
       <div className={styles.socials}>
         <i className="fab fa-facebook-f"></i>
